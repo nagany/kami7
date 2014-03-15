74cb42d7ff963c3d1e9064831674fff639352b14
tatsuya <nagany0209@gmail.com>
2014-03-15 23:23:41 +0900

    h

--- src/main/java/com/zaipon/kami7/dao/ConnectionUtility.java
+++ src/main/java/com/zaipon/kami7/dao/ConnectionUtility.java
@@ -17,14 +17,33 @@ public class ConnectionUtility {
 	    return DriverManager.getConnection(dbUrl, username, password);
 	}
 	
-		
+	public Connection createConnection(){
+
+		try {
+			// JDBCドライバの登録
+			String driver = "org.postgresql.Driver";
+			// データベースの指定
+			String server   = "ec2-54-225-101-119.compute-1.amazonaws.com";   // PostgreSQL サーバ ( IP または ホスト名 )
+			String dbname   = "dbc285t3b5t1rl";         // データベース名
+			String url = "jdbc:postgresql://ec2-54-225-101-119.compute-1.amazonaws.com:5432/dbc285t3b5t1rl";
+			String user     = "pxveoglutvfuzz";         //データベース作成ユーザ名
+			String password = "RsaIdqg7MXJmOMsXP3LwOxgsTt";     //データベース作成ユーザパスワード
+			Class.forName (driver);
+			// データベースとの接続
+			Connection conn = DriverManager.getConnection(url, user, password);
+			return conn;
+		} catch (SQLException e) {
+			System.err.println("SQL failed.");
+			e.printStackTrace ();
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace ();
+		}
+		return null;
+	}	
 
 	public void closeConnection(Connection conn){
 		try {
-			if(conn != null){
-
-				conn.close();
-			}
+			conn.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
