e4b11af5fcc152737add5bac41909c6eb76710fd
tatsuya <nagany0209@gmail.com>
2014-03-15 23:02:19 +0900

    h

--- src/main/java/com/zaipon/kami7/dao/ConnectionUtility.java
+++ src/main/java/com/zaipon/kami7/dao/ConnectionUtility.java
@@ -17,34 +17,14 @@ public class ConnectionUtility {
 	    return DriverManager.getConnection(dbUrl, username, password);
 	}
 	
-	
-//	
-//	public Connection createConnection(){
-//
-//		try {
-//			// JDBCドライバの登録
-//			String driver = "org.postgresql.Driver";
-//			// データベースの指定
-//			String server   = "kami7.herokuapp.com";   // PostgreSQL サーバ ( IP または ホスト名 )
-//			String dbname   = "postgres";         // データベース名
-//			String url = "jdbc:postgresql://kami7.herokuapp.com:5432/postgres";
-//			String user     = "nagany0209@gmail.com";         //データベース作成ユーザ名
-//			String password = "mina4799";     //データベース作成ユーザパスワード
-//			Class.forName (driver);
-//			// データベースとの接続
-//			Connection conn = DriverManager.getConnection(url, user, password);
-//			return conn;
-//		} catch (SQLException e) {
-//			System.err.println("SQL failed.");
-//			e.printStackTrace ();
-//		} catch (ClassNotFoundException ex) {
-//			ex.printStackTrace ();
-//		}
-//	}
+		
 
 	public void closeConnection(Connection conn){
 		try {
-			conn.close();
+			if(conn != null){
+
+				conn.close();
+			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
