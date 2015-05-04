e11878a00411e0dc627ee6019cf6969ac14b055c
tatsuya <nagany0209@gmail.com>
2015-05-04 19:12:33 +0900

    fix

--- src/main/java/StringTest.java
+++ src/main/java/StringTest.java
@@ -5,19 +5,16 @@ public class StringTest {
 		// TODO Auto-generated method stub
 		StringBuilder sb = new StringBuilder();
 		sb.append("select").append("\r\n");
-		sb.append("rate.member_id, member.member_name, member_image.image_path, rate.rate").append("\r\n");
-		sb.append("from").append("\r\n");
-		sb.append("member, rate, member_image").append("\r\n");
+		sb.append("rate.category_id, rate.member_id, rate.rate, member.member_name, category.category_id").append("\r\n");
+		sb.append("from rate").append("\r\n");
+		sb.append("inner join member on rate.member_id = member.member_id").append("\r\n");
+		sb.append("inner join category on rate.category_id = category.category_id").append("\r\n");
 		sb.append("where").append("\r\n");
-		sb.append("rate.member_id = member.member_id and").append("\r\n");
-		sb.append("member.member_id = member_image.member_id and").append("\r\n");
-		sb.append("rate.category_id = "+ 1 +" and").append("\r\n");//
+		sb.append("rate.category_id = "+ 1 +" and").append("\r\n");
 		sb.append("rate.delete_flag = false").append("\r\n");
-		sb.append("order by rate.member_id;").append("\r\n");
+		sb.append("order by rate.rate desc").append("\r\n");
 
-		
 		String sql = sb.toString();
-		
 		System.out.println(sql);
 	}
 
